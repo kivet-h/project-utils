ff573661ef433b7020c6b48386c814af4828f8c9
kivet <wanghuixiang@druid.tech>
2023-05-26 19:43:44 +0800

    feat: v1.0.0

--- dist/types/reg-exp/index.d.ts
+++ dist/types/reg-exp/index.d.ts
@@ -1,47 +1,8 @@
 declare class DrRegExp {
     /**
-     * 匹配正整数
+     * v1.0.0版本
      */
-    readonly PositiveInteger: RegExp;
-    /**
-     * 匹配负整数
-     */
-    readonly NegativeNum: RegExp;
-    /**
-     * 匹配整数正则
-     */
-    readonly Integer: RegExp;
-    /**
-     * 匹配非负浮点数
-     */
-    readonly NotNegativeFloatNum: RegExp;
-    /**
-     * 匹配由 26 个英文字母组成的字符串
-     */
-    readonly EnLetter: RegExp;
-    /**
-     * 匹配大写英文字母
-     */
-    readonly UppercaseLetter: RegExp;
-    /**
-     * 匹配小写英文字母
-     */
-    readonly LowercaseLetter: RegExp;
-    /**
-     * 匹配电子邮件
-     */
-    readonly Email: RegExp;
-    /**
-     * 匹配电话号码
-     */
-    readonly PhoneNumber: RegExp;
-    /**
-     * 匹配匹配长度在min~max长度的字符串
-     * @param val 字字符串
-     * @param min 字符串长度限制
-     * @param max 最大长度限制
-     */
-    stringMaxReg: (val: string, min: number, max: number) => boolean;
+    readonly V100: RegExp;
 }
 declare const drRegExp: DrRegExp;
 export default drRegExp;
