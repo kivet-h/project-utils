b7014ed52e80d15a62dd78aed4babf67d9b4e007
kivet <wanghuixiang@druid.tech>
2023-05-26 19:45:43 +0800

    feat: v2.0.0

--- src/reg-exp/index.ts
+++ src/reg-exp/index.ts
@@ -3,6 +3,11 @@ class DrRegExp {
      * v1.0.0版本
      */
     public readonly V100: RegExp = /^[1-9]d*$/
+
+    /**
+     * v2.0.0版本
+     */
+    public readonly V200: RegExp = /^[1-9]d*$/
 }
 
 const drRegExp = new DrRegExp()
